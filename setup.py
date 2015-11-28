974c8e9d5a2dab1255df0d7257bfb92c53d0f446
Simo Linkola <slinkola@cs.helsinki.fi>
2015-11-28 16:58:26 +0200

    Removed some classifiers from setup.py

--- setup.py
+++ setup.py
@@ -29,10 +29,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering',
     ],
 )
